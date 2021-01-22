0d8be1c241c8d35d8352e542825eb5bc700b8c10
mohanson <mohanson@outlook.com>
2021-01-22 12:12:06 +0800

    Refactor example max

--- example/max/main.go
+++ example/max/main.go
@@ -16,6 +16,15 @@ func f(x float64) float64 {
 	return math.Sin(10*x)*x + math.Cos(2*x)*x
 }
 
+func genemoDecode(g *ga.Genemo) float64 {
+	var c uint64 = 0
+	for i := 0; i < 10; i++ {
+		a := g.Locus[i] & 1
+		c |= a << i
+	}
+	return float64(ga.GraycodeDecode(c)) / 1023 * 5
+}
+
 func main() {
 	gas := ga.GAs{
 		Option: ga.GAsOption{
@@ -25,19 +34,14 @@ func main() {
 			PC:         0.5,
 			PM:         0.005,
 			Fitness: func(g *ga.Genemo) float64 {
-				var c uint64 = 0
-				for i := 0; i < 10; i++ {
-					a := g.Locus[i] & 1
-					c |= a << i
-				}
-				x := float64(ga.GraycodeDecode(c)) / 1023 * 5
-				return f(x)
+				return f(genemoDecode(g))
 			},
 			Trigger: func(g *ga.GAs) {
 				if g.Generation%10 == 0 {
 					log.Println("Generation", g.Generation)
 					i := ga.FindArgMax(g.Fitness)
-					log.Println("Individual", g.Option.Fitness(g.Population[i]))
+					individual := g.Population[i]
+					log.Printf("Individual x=%f f(x)=%f\n", genemoDecode(individual), g.Option.Fitness(individual))
 				}
 			},
 		},
